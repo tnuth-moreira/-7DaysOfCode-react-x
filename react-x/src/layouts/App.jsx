1d57e56b1f42f4fde89e9273c47f3f1810040a40
T.Nuth <thiagomoreira@Mac-Pro-de-Thiago.local>
2024-02-08 16:24:43 -0300

    Iniciando o projeto em react

---
+++ react-x/src/layouts/App.jsx
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types'
+
+export const App = ({ children }) => 
+ <>{children}</>;
+
+ App.propTypes = { 
+  children: PropTypes.node,
+ };
---
+++ react-x/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { App } from '../layouts/App'
+
+export const Home = () => ( 
+  <App>
+    <h1 className='text-xl text-sky-600'>
+      Home
+    </h1>
+  </App>
+)
